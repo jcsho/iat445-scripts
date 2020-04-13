c54551daa00a9e13b3c80041508aeaeafa734ee4
Justin Ho <justinhodev@gmail.com>
2020-04-12 21:29:11 -0700

    Modify Trap Object to Use Transform Velocity

--- Traps/TrapObject.cs
+++ Traps/TrapObject.cs
@@ -4,27 +4,13 @@ using UnityEngine;
 
 public class TrapObject : MonoBehaviour
 {
-
-    private Rigidbody rb;
     private Vector3 projectileDirection;
     private float projectileSpeed = 1f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (GetComponent<Rigidbody>() == null)
-        {
-            Debug.LogError("Trap Object requires Rigidbody component");
-        }
-        rb = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = projectileDirection.normalized * projectileSpeed;
-        // Debug.Log("Velocity: " + rb.velocity);
-        // Debug.Log("Direction: " + projectileDirection);
+        transform.Translate(projectileDirection.normalized * projectileSpeed * Time.deltaTime, Space.Self);
     }
 
     public void SetVelocity(float speed, Vector3 direction)
